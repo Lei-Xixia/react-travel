b9784af4bebd318b54f78cf95dccb5dda197dee9
Lei-Xixia <1377158174@qq.com>
2022-01-19 11:17:52 +0800

    通过路由导航实现首页渲染

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+import {
+  Header,
+  Footer,
+  SideMenu,
+  Carousel,
+  ProductCollection,
+  BusinessPartners,
+} from "../../components";
+import { Row, Col, Typography } from "antd";
+import { productList1, productList2, productList3 } from "./mockups";
+
+import sideImg1 from "../../assets/images/img1.png";
+import sideImg2 from "../../assets/images/img2.png";
+import sideImg3 from "../../assets/images/img3.png";
+
+export class HomePage extends React.Component {
+  render() {
+    return (
+      <>
+        <Header></Header>
+        {/* 页面内容content */}
+        <div className={styles["page-content"]}>
+          <Row style={{ marginTop: 20 }}>
+            <Col span={6}>
+              <SideMenu />
+            </Col>
+            <Col span={18}>
+              <Carousel />
+            </Col>
+          </Row>
+          <ProductCollection
+            title={
+              <Typography.Title level={3} type="warning">
+                爆款推荐
+              </Typography.Title>
+            }
+            sideImage={sideImg1}
+            products={productList1}
+          />
+          <ProductCollection
+            title={
+              <Typography.Title level={3} type="danger">
+                新品上市
+              </Typography.Title>
+            }
+            sideImage={sideImg2}
+            products={productList2}
+          />
+          <ProductCollection
+            title={
+              <Typography.Title level={3} type="success">
+                国内旅游推荐
+              </Typography.Title>
+            }
+            sideImage={sideImg3}
+            products={productList3}
+          />
+          <BusinessPartners />
+        </div>
+        <Footer></Footer>
+      </>
+    );
+  }
+}
