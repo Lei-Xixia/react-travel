b1f01cfc8d1be99d0d32f3801838ed5938e1d90d
Lei-Xixia <71595269+Lei-Xixia@users.noreply.github.com>
2022-02-13 13:03:03 +0800

    修改路由

---
+++ App.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import {
+  HomePage,
+  SignInPage,
+  RegisterPage,
+  DetailPage,
+  SearchPage,
+  ShoppingCartPage,
+  PlaceOrderPage,
+} from "./pages";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "./redux/hooks";
+import { useDispatch } from "react-redux";
+import { getShoppingCart } from "./redux/shoppingCart/slice";
+
+let NoMatch = () => {
+  return <p>404 出错啦!</p>;
+};
+
+const PrivateRoute = ({ isAuthenticated, children }) => {
+  return isAuthenticated ? children : <Navigate to={"/signIn"} />;
+};
+function App() {
+  const jwt = useSelector((s) => s.user.token);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getShoppingCart(jwt));
+    }
+  }, [jwt]);
+  return (
+    <div className={styles.App}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signIn" element={<SignInPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/detail/:touristRouteId" element={<DetailPage />} />
+          <Route path="/search/:keywords" element={<SearchPage />} />
+          <Route
+            path="/shoppingCart"
+            element={
+              <PrivateRoute isAuthenticated={jwt !== null}>
+                <ShoppingCartPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/placeOrder"
+            element={
+              <PrivateRoute isAuthenticated={jwt !== null}>
+                <PlaceOrderPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
